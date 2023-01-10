65f74b80bcfd536613eb65a438cc81d778c4daef
hongjuns <asws12345@naver.com>
2023-01-10 22:59:03 +0900

    commit

---
+++ src/test/java/com/example/demo/MySQLConnectionTest.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@SpringBootTest
+public class MySQLConnectionTest {
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    @Value("${spring.datasource.testurl}")
+    String URL;
+    @Value("${spring.datasource.username}")
+    String USER;
+    @Value("${spring.datasource.password}")
+    String PW;
+
+    @Test
+    public void testConnection() throws Exception{
+        Class.forName(DRIVER);
+
+        try(Connection conn = DriverManager.getConnection(URL, USER, PW)){
+            System.out.println("success : "  + conn);
+        } catch (Exception e) {
+            System.out.println("error :" +e.getMessage());
+        }
+    }
+}
